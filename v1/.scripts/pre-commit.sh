0bd60223c497ac8e4ce612d51a0bea359c4d4f9c
axier-iestyn <iestyn02@gmail.com>
2025-10-21 00:44:39 +0100

    test hooks

---
+++ v1/.scripts/pre-commit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+echo "🧩 Running v1 pre-commit logic..."
+# Example: npm run lint --workspace v1
---
+++ v2/.scripts/pre-commit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+echo "🧩 Running v2 pre-commit logic..."
+# Example: npm run lint --workspace v1
