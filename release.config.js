44da5eb7a6ca8cab7233fcd244b25da63e86a42a
axier-iestyn <iestyn02@gmail.com>
2025-10-21 17:22:28 +0100

    ci: added more hook configs and added .prettierrrc

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  types: [
+    {
+      type: 'feat',
+      release: 'minor'
+    },
+    {
+      type: 'fix',
+      release: 'patch'
+    },
+    {
+      type: 'chore',
+      release: false
+    }
+  ]
+};
