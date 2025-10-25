c38eee88b9b4bc0072e0d86bb1e509d9fb6593c7
axier-iestyn <iestyn02@gmail.com>
2025-10-25 01:36:34 +0100

    fix: fixed type issue

--- v1/src/main.ts
+++ v1/src/main.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 8746;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from v1!');
 });
 
